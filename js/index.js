acb11e51576084121126ec995134a47a527b7083
Michaelsp11 <michaelsalgadop11@gmail.com>
2021-06-07 13:10:54 +0200

    Se cargan y se imprimen personajes

--- js/index.js
+++ js/index.js
@@ -1,1 +1,33 @@
-
+import { getPersonajes } from "./funciones.js";
+// Elementos del DOM
+const personajeDummy = document.querySelector(".personaje-dummy");
+const personajesElemento = document.querySelector(".personajes");
+const btnCargaPersonajes = document.querySelector(".cargar-personajes");
+btnCargaPersonajes.addEventListener("click", () => {
+    (async() => {
+        const personajes = await getPersonajes();
+        pintarPersonajes(personajes);
+    })();
+});
+const vaciarPersonajes = () => {
+    for (const personajeElemento of personajesElemento.querySelectorAll(
+            ".personaje"
+        )) {
+        personajeElemento.remove();
+    }
+};
+const pintarPersonajes = (personajes) => {
+    vaciarPersonajes();
+    for (const { nombre, familia, vivo }
+        of personajes) {
+        const nuevoPersonaje = personajeDummy.cloneNode(true);
+        nuevoPersonaje.classList.remove("personaje-dummy");
+        const nombreNuevoPersonaje = nuevoPersonaje.querySelector(".nombre");
+        nombreNuevoPersonaje.textContent = nombre;
+        const familiaNuevoPersonaje = nuevoPersonaje.querySelector(".familia");
+        familiaNuevoPersonaje.textContent = familia;
+        const estadoNuevoPersonaje = nuevoPersonaje.querySelector(".estado");
+        estadoNuevoPersonaje.textContent = vivo ? "vivo" : "muerto";
+        personajesElemento.append(nuevoPersonaje);
+    }
+};
